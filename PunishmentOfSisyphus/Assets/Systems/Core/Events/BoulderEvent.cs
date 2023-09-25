ccf375abee392adf5252882b6617e7521f22137f
Brody Brewster <bpb4402@rit.edu>
2023-09-25 11:01:32 -0400

    adad

---
+++ PunishmentOfSisyphus/Assets/Systems/Core/Events/BoulderEvent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ephymeral.Events
+{
+    [CreateAssetMenu(fileName = "BoulderEvent", menuName = "ScriptableObjects/BoulderScriptableObj")]
+    public class BoulderEvent : ScriptableObject
+    {
+
+    }
+}
