f1ab751206959de256fac6a2f5199c1ab22e6709
Shaneols <70379061+Shaneols@users.noreply.github.com>
2023-11-02 13:32:23 -0400

    LevelChange half implemented

---
+++ PunishmentOfSisyphus/Assets/Systems/Core/Mechanics/Enemies/LevelChng.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Threading;
+
+using Ephymeral.EntityNS;
+using Ephymeral.BoulderNS;
+using Ephymeral.Events;
+using Ephymeral.Data;
+using Ephymeral.EnemyNS;
+using UnityEngine.SceneManagement;
+using Ephymeral.PlayerNS;
+
+public class LevelChng : MonoBehaviour
+{
+    private EnemySpawner enemySpawner;
+    private Player player;
+
+
+    private void Awake()
+    {
+        enemySpawner = (EnemySpawner)FindObjectOfType(typeof(EnemySpawner));
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("levelchng triggered");
+        if (collision.CompareTag("Player"))
+        {
+            if(enemySpawner.EnemiesCnt == 0)
+            {
+                enemySpawner.IncrementLevel();
+            }
+        }
+    }
+}
